8caf28b780f9b72d9e07bdf42bd375d5c85a4297
LEONARDO SOUZA LEANDRO <128157527+LEONARDO-LEANDRO@users.noreply.github.com>
2024-03-21 08:50:40 -0300

    Add files via upload

---
+++ teste1.js
@@ -0,0 +1,18 @@
+
+let idade = 15;
+let PossuiTituloEleitoral = true
+
+
+if(idade < 16 && PossuiTituloEleitoral)
+    console.log("Fih... esquece... vc ainda é só um bebê, volta pra casa chupar seu dedinho");
+
+else if(idade )
+    console.log("Você até pode votar, porém não é obrigatorio, ent bebê...deixa quieto, hihihihihihi!!")
+else if(idade >= 18 && PossuiTituloEleitoral){
+            console.log("Você pode votar e é obrigatório");
+}else{
+    console.log("Você não pode votar, bebê!!");
+}
+
+
+
